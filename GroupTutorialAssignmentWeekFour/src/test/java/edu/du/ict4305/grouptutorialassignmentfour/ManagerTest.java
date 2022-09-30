f0ec1d02922ed0398cbade7da87b222483b0dc5a
lutherchikumba <chikumbaluther@gmail.com>
2022-09-30 09:01:53 -0600

    Add methods to the class and Add test classes.

---
+++ GroupTutorialAssignmentWeekFour/src/test/java/edu/du/ict4305/grouptutorialassignmentfour/ManagerTest.java
@@ -0,0 +1,30 @@
+/**
+ *
+ * @Course: ICT 4305
+ * @File:
+ * @Instructor: Dr. Sherri Maciosek
+ *
+ */
+package edu.du.ict4305.grouptutorialassignmentfour;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+/**
+ * 
+ * @Date: 
+ * @author lutherchikumba
+ *
+ */
+public class ManagerTest {
+
+     public ManagerTest() {
+    }
+
+    /**
+     * Test of raiseSalary method, of class Manager.
+     */
+    @Test
+    public void testRaiseSalary() {
+    }
+
+}
