f63ec670e975fb994f6a28f03405184266ae28ca
lutherchikumba <chikumbaluther@gmail.com>
2022-10-26 18:29:58 +0800

    Add the tests to the project

---
+++ GroupTutorialAssignmentWeeSeven/src/test/java/edu/du/ict_4305_grouptutorialassignmentweekseven/PlayerTest.java
@@ -0,0 +1,34 @@
+package edu.du.ict_4305_grouptutorialassignmentweekseven; /**
+ * @Course: ICT 4305
+ * @Project: GroupTutorialAssignmentWeeSeven
+ * @Instructor: Dr. Sherri Maciosek
+ */
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @Date: 10/26/22
+ * @author lutherchikumba
+ *
+ */
+public class PlayerTest {
+
+      public PlayerTest() {
+
+      }
+
+      @Test
+      public void getName() {
+      }
+
+      @Test
+      public void getStats() {
+      }
+
+      @Test
+      public void getSport() {
+      }
+}
