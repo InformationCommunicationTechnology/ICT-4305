afc60a5761955430342e2705f40836488ba92997
lutherchikumba <chikumbaluther@gmail.com>
2022-11-08 21:11:06 -0700

    Add the files to the repo

---
+++ GroupTutorialAssignmentWeekNine/src/main/java/edu/du/ict4305/grouptutorialassignmentnine/Main.java
@@ -0,0 +1,8 @@
+package edu.du.ict4305.grouptutorialassignmentnine;
+
+public class Main {
+      public static void main(String[] args) throws Exception
+      {
+            Contact f = new Contact();
+      }
+}
